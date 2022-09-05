bd8f355a36648e57e2b801ee6b94d63dfe7dbfa0
btt08 <borjattrapiella@gmail.com>
2022-09-05 19:39:58 +0200

    Cambio de body-parser por middleware express. Razon: deprecated

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const productsRouter = require('./routes/productsRouter')
-const bp = require('body-parser');
 
 const config = require('./modules/config');
 const hostName = config.HOST;
@@ -11,8 +10,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
