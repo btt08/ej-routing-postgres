7f7c5bde67fe149f3183d896608b15300b93ea75
btt08 <borjattrapiella@gmail.com>
2022-09-05 19:22:55 +0200

    Arreglada query sobre el cambio del select

--- services/productsService.js
+++ services/productsService.js
@@ -6,7 +6,7 @@ const getAllProducts = async () => {
 };
 
 const filterProducts = async (brand, color, price, manufacturer) => {
-  let query = `SELECT * FROM products WHERE name LIKE '%${brand ? brand.toUpperCase() : ''}%' AND color LIKE '%${color ?? ''}%' ${price ? `AND price < ${price} ` : ''} AND cif LIKE '%${manufacturer ?? ''}%'`;
+  let query = `SELECT p.name AS brand, price, color, m.name AS Manufacturer, p.cif AS cif, addres AS Address FROM public.products p JOIN public.manufacturers m ON m.cif = p.cif WHERE p.name LIKE '%${brand ? brand.toUpperCase() : ''}%' AND p.color LIKE '%${color ?? ''}%' ${price ? `AND p.price < ${price} ` : ''} AND p.cif LIKE '%${manufacturer ?? ''}%'`;
   return await client.query(query);
 };
 
